01d9ddd482bb2e4c3c9416a95bb9bd2f0c20f734
Emin Arıkan <muhammeteminarikan@gmail.com>
2024-01-27 14:55:25 +0300

    changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,18 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard+=unnamedplus")
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = false
+
+vim.opt.scrolloff = 8
+
 vim.g.mapleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", '"_dP')
